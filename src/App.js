9383499d2b9f357fdb9c8924087446793f5e2f6d
Iago Cleber <iagocleber@gmail.com>
2023-01-26 15:00:27 -0300

    Estelizando e criando propriedades de ação para scroll na area de destaques.

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ export default()=>{
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1));
       let chosen = originals[0].items.results[randomChosen];
       let chosenInfo = await tmdb.getMovieInfo(chosen.id,'tv');
+      setfeatureData(chosenInfo);
     }
 
     loadAll();
--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -1,10 +1,21 @@
 import React from "react";
 import './FeatureMovie.css'
 
-export default () => {
+export default ({item}) => {
     return (
-        <div>
-            FILME EM DESTAQUE
-        </div>
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition:'center',
+            backgroundImage:`url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+
+        }}>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+
+                </div>
+
+            </div>
+
+        </section>
     );
 }
